9f1a791fb48a2ac7689d898e1b8277a203ae8a32
Kleberson Canuto <klebersoncanuto@gmail.com>
2024-12-17 18:15:55 -0300

    Initial commit

---
+++ ultraocr/constants.ts
@@ -0,0 +1,8 @@
+export const POOLING_INTERVAL = 1;
+export const API_TIMEOUT = 30;
+export const UPLOAD_TIMEOUT = 120;
+export const DEFAULT_EXPIRATION_TIME = 60;
+export const BASE_URL = "https://ultraocr.apis.nuveo.ai/v2";
+export const AUTH_BASE_URL = "https://auth.apis.nuveo.ai/v2";
+export const STATUS_DONE = "done";
+export const STATUS_ERROR = "error";
