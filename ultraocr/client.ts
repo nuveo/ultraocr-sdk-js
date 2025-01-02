d2a10190e7082b5a2d6371bc58159b13f65b779d
Kleberson Canuto <klebersoncanuto@gmail.com>
2025-01-02 16:47:45 -0300

    Add docs

--- ultraocr/client.ts
+++ ultraocr/client.ts
@@ -22,6 +22,8 @@ import { TimeoutError, InvalidStatusCodeError } from './errors';
 import { validateResponse, uploadFile, uploadFileWithPath } from './helpers';
 import { METHOD_GET, METHOD_POST } from './constants';
 
+// Client to help on UltraOCR usage. For more details about all arguments and returns,
+// access the oficial system documentation on https://docs.nuveo.ai/ocr/v2/.
 export class Client {
   clientID: string;
   clientSecret: string;
@@ -34,6 +36,10 @@ export class Client {
   token: string;
   expiresAt: Date;
 
+  /**
+   * The client constructor.
+   * @param {Config} config - The config specs.
+   */
   constructor(config: Config) {
     this.clientID = config?.clientID || '';
     this.clientSecret = config?.clientSecret || '';
@@ -89,11 +95,21 @@ export class Client {
       await this.authenticate(this.clientID, this.clientSecret, this.expires);
   }
 
+  /**
+   * Authenticate on UltraOCR and save the token to use on future requests.
+   * @param {string} clientID - The Client ID generated on Web Interface.
+   * @param {string} clientSecret - The Client Secret generated on Web Interface.
+   * @param {number} expires - The token expires time in minutes (Default 60).
+   * @returns {Promise<void>} A promise with success if authentication is ok.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * await client.authenticate("ID", "SECRET");
+   */
   public async authenticate(
     clientID: string,
     clientSecret: string,
     expires: number = DEFAULT_EXPIRATION_TIME,
-  ) {
+  ): Promise<void> {
     const url = `${this.authBaseUrl}/token`;
 
     const headers = {
@@ -121,6 +137,28 @@ export class Client {
     this.expiresAt = new Date(now.getTime() + expires * 60 * 1000);
   }
 
+  /**
+   * Generate signed url to send the document to be processed by the AI.
+   * @param {string} service - The the type of document to be sent.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @param {Resource} resource - The way to process, whether job or batch (Default job).
+   * @returns {Promise<UploadLinkResponse>} A promise with the upload link response.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const signedUrl = await client.generateSignedUrl("SERVICE", {}, {}, "job");
+   * console.log(signedUrl);
+   * // Output: {
+   * //           "exp": "60000",
+   * //           "id": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //           "status_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/batch/status/0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //           "urls": {
+   * //             "document": "https://presignedurldemo.s3.eu-west-2.amazonaws.com/image.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJJWZ7B6WCRGMKFGQ%2F20180210%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180210T171315Z&X-Amz-Expires=1800&X-Amz-Signature=12b74b0788aa036bc7c3d03b3f20c61f1f91cc9ad8873e3314255dc479a25351&X-Amz-SignedHeaders=host",
+   * //             "selfie": "https://presignedurldemo.s3.eu-west-2.amazonaws.com/image.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJJWZ7B6WCRGMKFGQ%2F20180210%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180210T171315Z&X-Amz-Expires=1800&X-Amz-Signature=12b74b0788aa036bc7c3d03b3f20c61f1f91cc9ad8873e3314255dc479a25351&X-Amz-SignedHeaders=host",
+   * //             "extra_document": "https://presignedurldemo.s3.eu-west-2.amazonaws.com/image.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJJWZ7B6WCRGMKFGQ%2F20180210%2Feu-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180210T171315Z&X-Amz-Expires=1800&X-Amz-Signature=12b74b0788aa036bc7c3d03b3f20c61f1f91cc9ad8873e3314255dc479a25351&X-Amz-SignedHeaders=host"
+   * //           }
+   * //         }
+   */
   public async generateSignedUrl(
     service: string,
     metadata: Record<string, any> = {},
@@ -135,6 +173,25 @@ export class Client {
     return res;
   }
 
+  /**
+   * Send job in a single step on UltraOCR, it's faster than usual method,
+   * but have a 6MB as body limit (including metadata and base64 file).
+   * @param {string} service - The the type of document to be sent.
+   * @param {string} file - The file in base64 format.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @param {string} facematchFile - The facematch file in base64 format.
+   * @param {string} extraFile - The extra file in base64 format.
+   * @returns {Promise<CreatedResponse>} A promise with the job info to get it status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const jobCreated = await client.sendJobSingleStep("SERVICE", "FILE");
+   * console.log(jobCreated);
+   * // Output: {
+   * //           "id": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //           "status_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/job/result/0ujsszwN8NRY24YaXiTIE2VWDTS"
+   * //         }
+   */
   public async sendJobSingleStep(
     service: string,
     file: string,
@@ -158,6 +215,24 @@ export class Client {
     return res;
   }
 
+  /**
+   * Create and upload a job.
+   * @param {string} service - The the type of document to be sent.
+   * @param {string} filePath - The file path of the document.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @param {string} facematchFilePath - The facematch file path of the document.
+   * @param {string} extraFilePath - The extra file path of the document.
+   * @returns {Promise<CreatedResponse>} A promise with the job info to get it status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const jobCreated = await client.sendJob("SERVICE", "FILE_PATH");
+   * console.log(jobCreated);
+   * // Output: {
+   * //           "id": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //           "status_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/job/result/0ujsszwN8NRY24YaXiTIE2VWDTS"
+   * //         }
+   */
   public async sendJob(
     service: string,
     filePath: string,
@@ -188,6 +263,22 @@ export class Client {
     };
   }
 
+  /**
+   * Create and upload a batch.
+   * @param {string} service - The the type of document to be sent.
+   * @param {string} filePath - The file path of the document.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @returns {Promise<CreatedResponse>} A promise with the batch info to get it status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const batchCreated = await client.sendBatch("SERVICE", "FILE_PATH");
+   * console.log(batchCreated);
+   * // Output: {
+   * //           "id": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //           "status_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/batch/status/0ujsszwN8NRY24YaXiTIE2VWDTS"
+   * //         }
+   */
   public async sendBatch(
     service: string,
     filePath: string,
@@ -206,6 +297,25 @@ export class Client {
     };
   }
 
+  /**
+   * Create and upload a job on base64 format (recommended only if you have
+   * already converted the file to base64 format).
+   * @param {string} service - The the type of document to be sent.
+   * @param {string} file - The file in base64 format.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @param {string} facematchFile - The facematch file in base64 format.
+   * @param {string} extraFile - The extra file in base64 format.
+   * @returns {Promise<CreatedResponse>} A promise with the job info to get it status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const jobCreated = await client.sendJobBase64("SERVICE", "FILE");
+   * console.log(jobCreated);
+   * // Output: {
+   * //           "id": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //           "status_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/job/result/0ujsszwN8NRY24YaXiTIE2VWDTS"
+   * //         }
+   */
   public async sendJobBase64(
     service: string,
     file: string,
@@ -237,6 +347,23 @@ export class Client {
     };
   }
 
+  /**
+   * Create and upload a batch on base64 format (recommended only if you have
+   * already converted the file to base64 format).
+   * @param {string} service - The the type of document to be sent.
+   * @param {string} file - The file in base64 format.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @returns {Promise<CreatedResponse>} A promise with the batch info to get it status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const batchCreated = await client.sendBatchBase64("SERVICE", "FILE");
+   * console.log(batchCreated);
+   * // Output: {
+   * //           "id": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //           "status_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/batch/status/0ujsszwN8NRY24YaXiTIE2VWDTS"
+   * //         }
+   */
   public async sendBatchBase64(
     service: string,
     file: string,
@@ -256,6 +383,29 @@ export class Client {
     };
   }
 
+  /**
+   * Get the status of the batch, checking whether it was processed or not.
+   * @param {string} batchID - The id of the batch, given on batch creation.
+   * @returns {Promise<BatchStatusResponse>} A promise with the batch status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const batch = await client.getBatchStatus("ID");
+   * console.log(batch);
+   * // Output: {
+   * //           "batch_ksuid": "2AwrSd7bxEMbPrQ5jZHGDzQ4qL3",
+   * //           "created_at": "2022-06-22T20:58:09Z",
+   * //           "jobs": [
+   * //             {
+   * //               "created_at": "2022-06-22T20:58:09Z",
+   * //               "job_ksuid": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //               "result_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/job/result/2AwrSd7bxEMbPrQ5jZHGDzQ4qL3/0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //               "status": "processing"
+   * //             }
+   * //           ],
+   * //           "service": "cnh",
+   * //           "status": "done"
+   * //         }
+   */
   public async getBatchStatus(batchID: string): Promise<BatchStatusResponse> {
     const url = `${this.baseUrl}/ocr/batch/status/${batchID}`;
     const response = await this.get(url);
@@ -265,6 +415,37 @@ export class Client {
     return res;
   }
 
+  /**
+   * Get the status and result of the job if it's already processed.
+   * @param {string} batchID - The id of the batch, given on batch creation(repeat the job_id if batch wasn't created).
+   * @param {string} jobID - The id of the job, given on job creation or on batch status.
+   * @returns {Promise<JobResultResponse>} A promise with the job result.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @example
+   * const job = await client.getJobResult("ID", "ID");
+   * console.log(job);
+   * // Output: {
+   * //           "client_data": { },
+   * //           "created_at": "2022-06-22T20:58:09Z",
+   * //           "job_ksuid": "2AwrSd7bxEMbPrQ5jZHGDzQ4qL3",
+   * //           "result": {
+   * //             "Time": "7.45",
+   * //             "Document": [
+   * //               {
+   * //                 "Page": 1,
+   * //                 "Data": {
+   * //                   "DocumentType": {
+   * //                     "conf": 99,
+   * //                     "value": "CNH"
+   * //                   }
+   * //                 }
+   * //               }
+   * //             ]
+   * //           },
+   * //           "service": "idtypification",
+   * //           "status": "done"
+   * //         }
+   */
   public async getJobResult(batchID: string, jobID: string): Promise<JobResultResponse> {
     const url = `${this.baseUrl}/ocr/job/result/${batchID}/${jobID}`;
     const response = await this.get(url);
@@ -284,14 +465,14 @@ export class Client {
    * const jobs = await client.getJobs("2024-01-01", "2024-01-02");
    * console.log(jobs);
    * // Output: [
-   * //         {
-   * //           created_at: "2021-01-01T00:00:00Z",
-   * //           job_ksuid: "21eRubs77luzFr1GdJfHdddH6EA",
-   * //           result: {},
-   * //           service: "chn",
-   * //           status: "done"
-   * //         }
-   * //       ]
+   * //           {
+   * //             created_at: "2021-01-01T00:00:00Z",
+   * //             job_ksuid: "21eRubs77luzFr1GdJfHdddH6EA",
+   * //             result: {},
+   * //             service: "chn",
+   * //             status: "done"
+   * //           }
+   * //         ]
    */
   public async getJobs(start: string, end: string): Promise<JobResultResponse[]> {
     const params = {
@@ -317,6 +498,38 @@ export class Client {
     return jobs;
   }
 
+  /**
+   * Wait the job to be processed and returns the result.
+   * @param {string} batchID - The id of the batch, given on batch creation(repeat the job_id if batch wasn't created).
+   * @param {string} jobID - The id of the job, given on job creation or on batch status.
+   * @returns {Promise<JobResultResponse>} A promise with the job result.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @throws {TimeoutError} If wait time exceed the limit.
+   * @example
+   * const job = await client.waitForJobDone("ID", "ID");
+   * console.log(job);
+   * // Output: {
+   * //           "client_data": { },
+   * //           "created_at": "2022-06-22T20:58:09Z",
+   * //           "job_ksuid": "2AwrSd7bxEMbPrQ5jZHGDzQ4qL3",
+   * //           "result": {
+   * //             "Time": "7.45",
+   * //             "Document": [
+   * //               {
+   * //                 "Page": 1,
+   * //                 "Data": {
+   * //                   "DocumentType": {
+   * //                     "conf": 99,
+   * //                     "value": "CNH"
+   * //                   }
+   * //                 }
+   * //               }
+   * //             ]
+   * //           },
+   * //           "service": "idtypification",
+   * //           "status": "done"
+   * //         }
+   */
   public async waitForJobDone(batchID: string, jobID: string): Promise<JobResultResponse> {
     const now = new Date();
     const limit = new Date(now.getTime() + this.timeout * 1000);
@@ -341,6 +554,32 @@ export class Client {
     return res;
   }
 
+  /**
+   * Wait the batch to be processed and returns the status. The function will
+   * wait the timeout given on Client creation.
+   * @param {string} batchID - The id of the batch, given on batch creation.
+   * @param {boolean} waitJobs - Indicate if must wait the jobs to be processed (default true).
+   * @returns {Promise<BatchStatusResponse>} A promise with the batch status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @throws {TimeoutError} If wait time exceed the limit.
+   * @example
+   * const batch = await client.waitForBatchDone("ID");
+   * console.log(batch);
+   * // Output: {
+   * //           "batch_ksuid": "2AwrSd7bxEMbPrQ5jZHGDzQ4qL3",
+   * //           "created_at": "2022-06-22T20:58:09Z",
+   * //           "jobs": [
+   * //             {
+   * //               "created_at": "2022-06-22T20:58:09Z",
+   * //               "job_ksuid": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //               "result_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/job/result/2AwrSd7bxEMbPrQ5jZHGDzQ4qL3/0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //               "status": "processing"
+   * //             }
+   * //           ],
+   * //           "service": "cnh",
+   * //           "status": "done"
+   * //         }
+   */
   public async waitForBatchDone(
     batchID: string,
     waitJobs: boolean = true,
@@ -375,6 +614,42 @@ export class Client {
     return res;
   }
 
+  /**
+   * Create the job and wait for job done.
+   * @param {string} service - The the type of document to be sent.
+   * @param {string} filePath - The file path of the document.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @param {string} facematchFilePath - The facematch file path of the document.
+   * @param {string} extraFilePath - The extra file path of the document.
+   * @returns {Promise<JobResultResponse>} A promise with the job result.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @throws {TimeoutError} If wait time exceed the limit.
+   * @example
+   * const job = await client.createAndWaitJob("SERVICE", "FILE_PATH");
+   * console.log(job);
+   * // Output: {
+   * //           "client_data": { },
+   * //           "created_at": "2022-06-22T20:58:09Z",
+   * //           "job_ksuid": "2AwrSd7bxEMbPrQ5jZHGDzQ4qL3",
+   * //           "result": {
+   * //             "Time": "7.45",
+   * //             "Document": [
+   * //               {
+   * //                 "Page": 1,
+   * //                 "Data": {
+   * //                   "DocumentType": {
+   * //                     "conf": 99,
+   * //                     "value": "CNH"
+   * //                   }
+   * //                 }
+   * //               }
+   * //             ]
+   * //           },
+   * //           "service": "idtypification",
+   * //           "status": "done"
+   * //         }
+   */
   public async createAndWaitJob(
     service: string,
     filePath: string,
@@ -395,6 +670,34 @@ export class Client {
     return this.waitForJobDone(jobID, jobID);
   }
 
+  /**
+   * Create the batch and wait for batch done.
+   * @param {string} service - The the type of document to be sent.
+   * @param {string} filePath - The file path of the document.
+   * @param {Record<string, any>} metadata - The metadata based on UltraOCR Docs format, optional in most cases.
+   * @param {Record<string, string>} params - The query parameters based on UltraOCR Docs, optional in most cases.
+   * @param {boolean} waitJobs - Indicate if must wait the jobs to be processed (default true).
+   * @returns {Promise<BatchStatusResponse>} A promise with the batch status.
+   * @throws {InvalidStatusCodeError} If request fail.
+   * @throws {TimeoutError} If wait time exceed the limit.
+   * @example
+   * const batch = await client.createAndWaitBatch("SERVICE", "FILE_PATH");
+   * console.log(batch);
+   * // Output: {
+   * //           "batch_ksuid": "2AwrSd7bxEMbPrQ5jZHGDzQ4qL3",
+   * //           "created_at": "2022-06-22T20:58:09Z",
+   * //           "jobs": [
+   * //             {
+   * //               "created_at": "2022-06-22T20:58:09Z",
+   * //               "job_ksuid": "0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //               "result_url": "https://ultraocr.apis.nuveo.ai/v2/ocr/job/result/2AwrSd7bxEMbPrQ5jZHGDzQ4qL3/0ujsszwN8NRY24YaXiTIE2VWDTS",
+   * //               "status": "processing"
+   * //             }
+   * //           ],
+   * //           "service": "cnh",
+   * //           "status": "done"
+   * //         }
+   */
   public async createAndWaitBatch(
     service: string,
     filePath: string,
--- ultraocr/constants.ts
+++ ultraocr/constants.ts
@@ -1,12 +1,22 @@
+/** @const {number} POOLING_INTERVAL - The pooling interval (in seconds) */
 export const POOLING_INTERVAL = 1;
+/** @const {number} API_TIMEOUT - The pooling timeout (in seconds) */
 export const API_TIMEOUT = 30;
-export const UPLOAD_TIMEOUT = 120;
+/** @const {number} DEFAULT_EXPIRATION_TIME - The token expiration time (in minutes) */
 export const DEFAULT_EXPIRATION_TIME = 60;
+/** @const {string} BASE_URL - The UltraOCR base URL */
 export const BASE_URL = 'https://ultraocr.apis.nuveo.ai/v2';
+/** @const {string} BASE_URL - The UltraOCR authentication base URL */
 export const AUTH_BASE_URL = 'https://auth.apis.nuveo.ai/v2';
+/** @const {string} STATUS_DONE - The UltraOCR status done */
 export const STATUS_DONE = 'done';
+/** @const {string} STATUS_ERROR - The UltraOCR status error */
 export const STATUS_ERROR = 'error';
+/** @const {string} METHOD_GET - The HTTP GET method */
 export const METHOD_GET = 'GET';
+/** @const {string} METHOD_POST - The HTTP POST method */
 export const METHOD_POST = 'POST';
+/** @const {string} METHOD_PUT - The HTTP PUT method */
 export const METHOD_PUT = 'PUT';
+/** @const {string} UTF8 - The UTF-8 encoding */
 export const UTF8 = 'utf-8';
